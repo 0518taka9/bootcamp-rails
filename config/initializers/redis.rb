12f26dcf7dbce829535599117893a5f8e2e0403b
0518taka9 <sunfkingdom96@gmail.com>
2018-02-07 16:00:15 +0900

    set heroku-redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 # REDIS = Redis.new(host: "localhost", port: 6379)
-REDIS = Redis.new(url: "redis://h:pe542c2e3f0ae3205be27411fefdbd6ea899369aa2adbd4d08808c3e69e1bee47@ec2-34-206-56-163.compute-1.amazonaws.com:63619")
+REDIS = Redis.new(url: "redis://h:p1e646514537d6631ea34c79af1a150f9a47c0bc4c5a8caa3e73a06d1a33b1a24@ec2-52-5-111-41.compute-1.amazonaws.com:53539")
