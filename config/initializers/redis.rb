1e7cb712eb1e0d5848fede5814fd2162dfaa0671
0518taka9 <sunfkingdom96@gmail.com>
2018-02-07 17:43:12 +0900

    set access permission

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 # REDIS = Redis.new(host: "localhost", port: 6379)
-REDIS = Redis.new(url: "redis://h:p1e646514537d6631ea34c79af1a150f9a47c0bc4c5a8caa3e73a06d1a33b1a24@ec2-52-5-111-41.compute-1.amazonaws.com:53539")
+REDIS = Redis.new(url: "redis://h:p1e646514537d6631ea34c79af1a150f9a47c0bc4c5a8caa3e73a06d1a33b1a24@ec2-52-5-111-41.compute-1.amazonaws.com:53539heroku")
