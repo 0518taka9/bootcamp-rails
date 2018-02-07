b765cc985c76f3d1e66ec2d49f88dd074228c8a7
0518taka9 <sunfkingdom96@gmail.com>
2018-02-07 20:34:55 +0900

    modify detail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :uid, presence: true, uniqueness: { case_sensitive: false }, format: { with: /@[a-z\d\-.]+/ }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :authentication_keys => [:login]
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(uid) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:uid].nil?
+        where(conditions).first
+      else
+        where(uid: conditions[:uid]).first
+      end
+    end
+  end
 end
